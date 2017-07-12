db90766fdc3be423baf5b4ce8f1a20b65f07d311
n8sters <ncpowers93@gmail.com>
2017-07-12 14:23:28 -0700

    minor UI changes

--- app/src/main/java/com/example/natepowers/driverapitoyapp/DriverApi.java
+++ app/src/main/java/com/example/natepowers/driverapitoyapp/DriverApi.java
@@ -1,14 +1,8 @@
 package com.example.natepowers.driverapitoyapp;
 
-import java.util.List;
-
 import retrofit2.Call;
-import retrofit2.Callback;
 import retrofit2.http.Body;
-import retrofit2.http.GET;
-import retrofit2.http.Headers;
 import retrofit2.http.POST;
-import retrofit2.http.Query;
 
 
 /**
@@ -27,27 +21,6 @@ public interface DriverApi {
     @POST("/sessions/authenticate/request")
     Call<User> createAccount(@Body User user);
 
-    @POST("/sessions/authenticate/request")
-    void getAuthRequestFromApi(
-            Callback<DriverData> callback);
 
-    @POST("/sessions/authenticate")
-    void logDriverIn(
-            @Query("phone") String phone,
-            Callback<DriverData> callback);
-
-
-    // is this right?
-    interface getDriverInfo {
-        @Headers("Username:UUID")
-        @GET("/driver")
-        List<DriverData> getTasks();
-    }
-
-    /*   Example auth request from SO
-
-    @GET("/secret_things")
-    SecretThing.List getSecretThings(@Header("Authorization") String token)
-    */
 
 }
